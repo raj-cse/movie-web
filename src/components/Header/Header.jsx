61b316e21ee5df07b3d96b76a8fd52934a119a03
RAJ PATIYAL <rajpatiyal26@gmail.com>
2024-06-22 15:55:02 +0530

    web

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import logo from '../../Image/ffTiYckswWi9iJ3Oj4vRm-transformed.png';
 import searchIcon from '../../Image/logo.svg';
+import hamburgerIcon from '../../Image/icons8-line-width-24-removebg-preview.png';
 
 export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className='header'>
-      <div className='header-left'>
+      <div className='left-container'>
         <img className='header-icon' src={logo} alt="Movie Zone Logo" />
-        <ul className='nav-list'>
+        <ul className={`nav-list ${isMenuOpen ? 'open' : ''}`}>
           <li className='nav-item'>Home</li>
           <li className='nav-item'>Genre</li>
           <li className='nav-item'>Movies</li>
@@ -16,12 +23,22 @@ export const Header = () => {
         </ul>
       </div>
 
-      <div className='search-box'>
-        <input type="text" placeholder='Search' />
-        <img src={searchIcon} alt="Search" className='search-icon' />
+      <div className='right-container'>
+        <div className='search-box'>
+          <input type="text" placeholder='Search' />
+          <img src={searchIcon} alt="Search" className='search-icon' />
+        </div>
+        
         <div className='sign-in'>
           <button className='sign-button'>Sign In</button>
         </div>
+
+        <img
+          src={hamburgerIcon}
+          alt="Hamburger Menu"
+          className='hamburger-icon'
+          onClick={toggleMenu}
+        />
       </div>
     </div>
   );
