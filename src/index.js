4a836c09258ed4b3e144d3ef38c62174383f333a
RAJ PATIYAL <rajpatiyal26@gmail.com>
2024-06-26 22:52:34 +0530

    abc

--- src/index.js
+++ src/index.js
@@ -6,16 +6,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import Routeall from './components/Routes/Routes';// Make sure the component is correctly named
 import Pages from './components/Pages/Pages';
+import NotFound from './Pages/Not-found/NotFound';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<App />} />
-          <Route exact path="/" element={<App />} />
+          <Route exact path="/" element={<App />} />   
           <Route path="/movies/:id" element={<Routeall />} />
           <Route path="/search/:query" element={<Pages />} />
+          <Route path="/*" element={<NotFound/>} />
+         
         </Routes>
       </BrowserRouter>
     </React.StrictMode>
